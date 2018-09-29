f13406bc7d560b543b796e355f1365b898d2a3fb
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-09-28 21:29:08 -0400

    Modified server.js

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+//mongoose.Promise = Promise;
 
 const app = express();
 
 //DB config
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI;
+console.log(db);
+//Connect to MongoDB through mongoose
+// mongoose.connect(db)
+//     .then(() => {
+//         mongoose.connection.on('error', err => {
+//             console.log('mongoose connection error: '+err);
+//         });
 
-//Connect to MongoDB
-mongoose
-  .connect(db)
-  .then(() => {console.log("MongoDB connected")})
-  .catch(err => console.log(err));
+//         console.log('connected - attempting reconnect');
+//         return mongoose.connect(url);
+//     })
+//     .catch(err => {
+//         console.log('rejected promise: '+err);
+//         mongoose.disconnect();
+//     });
 
+
+
+
+  mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then( () => console.log('MongoDB Connected'))
+    .catch(err => console.log(err)); 
+ 
 app.get("/", (req, res) => res.send("Hello World!"));
 
 const port = process.env.PORT || 5000;
