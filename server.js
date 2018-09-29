78c314d9fc8aec0f2947631caf5ac948cde57500
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-09-28 21:48:50 -0400

    Connection to database successful

--- server.js
+++ server.js
@@ -7,30 +7,14 @@ const app = express();
 //DB config
 const db = require('./config/keys').mongoURI;
 console.log(db);
-//Connect to MongoDB through mongoose
-// mongoose.connect(db)
-//     .then(() => {
-//         mongoose.connection.on('error', err => {
-//             console.log('mongoose connection error: '+err);
-//         });
 
-//         console.log('connected - attempting reconnect');
-//         return mongoose.connect(url);
-//     })
-//     .catch(err => {
-//         console.log('rejected promise: '+err);
-//         mongoose.disconnect();
-//     });
-
-
-
-
+//connect to mongoDB
   mongoose
-    .connect(db, { useNewUrlParser: true })
-    .then( () => console.log('MongoDB Connected'))
+    .connect(db)
+    .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err)); 
  
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get('/', (req, res) => res.send('Hello World!'));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
