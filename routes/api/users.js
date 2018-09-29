77716ac31583d15096e2d9acdf80fce45529b3e3
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-09-29 19:33:25 -0400

    Working on Post route to find a user in DB

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,14 @@ const router = express.Router();
 // @access     Public
 router.get('/test', (req, res) => res.json({msg: "Users route works"}));
 
+// @route      GET api/users/register
+// @desc       Register user
+// @access     Public
+router.post('/register', (req, res) => {
+    //check if user already exists
+    
+});
+
 //export router for server.js to pick it up
 module.exports = router;
 
