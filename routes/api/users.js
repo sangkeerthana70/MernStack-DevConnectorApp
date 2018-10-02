75bc5d0f40c1fdb81676213b876a4b9b5719b3ba
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-10-02 11:18:44 -0400

    Installed gravatar to be used in the Users model if available

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
 
 // Bring in user model
 const User = require('../../models/User')
@@ -19,6 +20,12 @@ router.post('/register', (req, res) => {
             if(user) {
                 return res.status(400).json({email: 'Email already exists'});
             } else {
+                const avatar = gravatar.url(req.body.email, {
+                    s: '200', //Size of avatar
+                    r: 'pg', // Rating
+                    d: 'mm' //Default
+                });
+
                 const newUser = new User({//else add new user to the User model in DB
                     name: req.body.name,
                     email: req.body.email,
