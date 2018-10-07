c8b2773e8ee6dd6cb74bb13c3a5fc80d3193e08f
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-10-07 17:52:24 -0400

    Validation-1 modified users.js register route to validateRegisterInput of user to check for undefined, null, empty object or empty string values

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
+// Load Input validation
+const validateRegisterInput = require('../../validation/register');
+
 // Bring in user model
 const User = require('../models/User');
 
@@ -18,6 +21,12 @@ router.get('/test', (req, res) => res.json({msg: "Users route works"}));
 // @desc       Register user
 // @access     Public
 router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+    
+    //Check Validation
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
     //check if user already exists my matching user email with email in DB
     User.findOne({ email: req.body.email})
         .then(user => {
