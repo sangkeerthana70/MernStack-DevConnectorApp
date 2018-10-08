a722286bf7cd09f88cd915edae70fea58fdce820
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-10-08 17:27:01 -0400

    User registration successful through Postman

--- validation/register.js
+++ validation/register.js
@@ -10,44 +10,45 @@ module.exports = function validateRegisterInput(data) {
     data.password2 = !isEmpty(data.password2) ? data.password2 : '';
 
     // Validate length of name field
-    if(!Validator.isLength(data.name, { min: 2, max: 30 })){
+    if (!Validator.isLength(data.name, { min: 2, max: 30 })){
         errors.name = 'Name must be between 2 and 30 characters';
     }
 
     //Validate if name field is empty
-    if(Validator.isEmpty(data.name)) {
+    if (Validator.isEmpty(data.name)) {
         errors.name = 'Name field is required';
     }
 
     //Validate if email field is empty
-    if(Validator.isEmpty(data.email)) {
+    if (Validator.isEmpty(data.email)) {
         errors.email = 'Email field is required';
     }
 
     // Check if valid email
-    if(Validator.isEmail(data.email)) {
+    if (!Validator.isEmail(data.email)) {
         errors.email = 'Email is invalid';
     }
 
     //Check for empty password
-    if(Validator.isEmpty(data.password)) {
+    if (Validator.isEmpty(data.password)) {
         errors.password = 'Password field is required';
     }
 
     //Check for Password length and strength
-    if(Validator.isLength(data.password, { min:6, max: 30 })) {
+    if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
         errors.password = 'Password must be at least 6 characters';
     }
 
     //Check for Password2 is empty
-    if(Validator.isLength(data.password2)) {
+    if (Validator.isEmpty(data.password2)) {
         errors.password2 = 'Confirm Password field is required';
     }
     
-    //Cnonfirm password and password2 are same
-    if(Validator.equals(data.password, data.password2)) {
+    //Confirm password and password2 are same
+    if (!Validator.equals(data.password, data.password2)) {
         errors.password2 = 'Passwords must match';
     }
+
     return {
         errors: errors,
         isValid: isEmpty(errors)//import isEmpty.js
