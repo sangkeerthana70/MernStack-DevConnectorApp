760c3b3577211ea9add50097b0a6686c08e7697b
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-10-09 17:36:08 -0400

    Create a POST route in profile.js to create or edit a user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -34,6 +34,52 @@ router.get(
     }
 );
 
+// @route      POST api/profile
+// @desc       Create or Edit user profile 
+// @access     Private
+router.post(
+    '/', 
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        // Get fields
+        const profileFields = {};
+        profileFields.user = req.user.id;
+        if(req.body.handle) profileFields.handle = req.body.handle;
+        if(req.body.company) profileFields.company = req.body.company;
+        if(req.body.website) profileFields.website = req.body.website;
+        if(req.body.location) profileFields.location = req.body.location;
+        if(req.body.bio) profileFields.bio = req.body.bio;
+        if(req.body.status) profileFields.status = req.body.status;
+        if(req.body.githubusername) profileFields.githubusername = req.body.githubusername;
+        // Skills - Split into array
+        if(typeof req.body.skills !== 'undefined') {
+            profileFields.skills = req.body.skills.split(',');
+        }
+
+        // Social
+        profileFields.social = {};
+        if(req.body.youtube) profileFields.social.youtube = req.body.youtube;
+        if(req.body.twitter) profileFields.social.twitter = req.body.twitter;
+        if(req.body.facebook) profileFields.social.facebook = req.body.facebook;
+        if(req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+        if(req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+        Profile.findOne({ user: req.user.id })
+            .then(profile => {
+                if(profile) {
+                    //if user profile exists then update
+                    Profile.findOneAndUpdate(
+                        { user: req.user.id },
+                        { $set: profileFields },
+                        { new: true }
+                        );
+                } else {
+
+                }
+            })
+        
+    }
+);
 //export router for server.js to pick it up
 module.exports = router;
 
