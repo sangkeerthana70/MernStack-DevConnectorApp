4b00ece447bfbf8e7e794affc0fed131bd3802b1
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-10-09 00:15:46 -0400

    Create a route for current User Profile in profile.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -10,7 +10,7 @@ const passport = require('passport');
 const validateRegisterInput = require('../../validation/register');
 const validateLoginInput = require('../../validation/login');
 // Bring in user model
-const User = require('../models/User');
+const User = require('../../models/User');
 
 // @route      GET api/users/test
 // @desc       Tests users route
